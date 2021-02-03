dc70bd6235ba50c5b86257038246b97da4b6f784
lbult <lennart.bult10@gmail.com>
2021-02-03 11:01:39 +0100

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -11,18 +11,30 @@ def answer(theta):
 def second(theta):
     return np.sin(theta-pi/2) + 1 + np.cos(theta-pi/2)*(4-abs(theta))
 
+def one(theta):
+    return np.cos(theta-pi/2) - np.sin(theta-pi/2)*(4-abs(theta))
+
+def two(theta):
+    return -np.sin(theta-pi/2) - 1 - np.cos(theta-pi/2)*(4-abs(theta))
+
 zeta = 0
 f = 0
 x =[]
 y =[]
 
+k = []
+r = []
+
 while f < 1000:
     x.append(answer(zeta))
     y.append(second(zeta))
+    k.append(one(zeta))
+    r.append(two(zeta))
     zeta += pi/1000
     f+=1
 
 plt.plot(x,y, 'r')
+plt.plot(k,r, 'r')
 
 # show the plot
 plt.show()
