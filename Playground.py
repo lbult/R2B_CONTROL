ebed12345e17af18790a7ebb9eafa7479af9a235
lbult <lennart.bult10@gmail.com>
2021-02-09 14:56:57 +0100

    Trajectory Planning
    Some improvement made, however, not yet finished

--- Playground.py
+++ Playground.py
@@ -38,3 +38,54 @@ plt.plot(k,r, 'r')
 
 # show the plot
 plt.show()
+
+"""
+Store:
+dtheta_r = -arclengths[0]
+    theta_r = pi + dtheta_r
+    dtheta_l = -arclengths[2]
+    theta_l= -pi/2 + dtheta_l
+
+    print(dtheta_l)
+    print(dtheta_r)
+
+    def func_1():
+        my_one = 0
+        interval_one = arclengths[0]/(1000)
+        my_theta_r = pi
+        while my_one < 1000:
+            x_1l.append(init_conditions[0] + r + r*cos(my_theta_r))
+            y_1l.append(init_conditions[1] + r*sin(my_theta_r))
+            my_theta_r += interval_one
+            my_one += 1
+
+    def func_2():
+        my_two = 0
+        interval_two = arclengths[2]/(1000)
+        my_theta_l = -pi/2
+        while my_two < 1000:
+            x_2l.append(r*cos(my_theta_l ))
+            y_2l.append(r + r*sin(my_theta_l))
+            my_theta_l += interval_two
+            my_two += 1
+
+    def func_3():
+        dy = y_2l[-1] - y_1l[-1]
+        dx = x_2l[-1] - x_1l[-1]
+        a = dy/dx
+        b = y_1l[-1] - dy/dx * x_1l[-1]
+
+        x_33 = x_1l[-1]
+
+        my_three = 0
+        interval_three = dx/1000
+        while my_three < 1000:
+            y_3l.append(+dy/dx*x_33+b)
+            x_3l.append(x_33)
+            x_33 += interval_three
+            my_three += 1
+
+    func_1()
+    func_2()
+    func_3()
+"""
--- Wind_Reference_System.py
+++ Wind_Reference_System.py
@@ -11,11 +11,17 @@ def _Gen_Wind_Field(x_speed, y_speed):
 def _Return_Minimum_Value(values):
     return min(values)
 
-def _RSL(lambdas, d, r_traj, gamma_traj, gamma_g_traj):
-    p_rsl = (-2*r_traj**2 + d**2 + 2*r_traj**2*cos(lambdas) + 2*r_traj*d*(sin(lambdas)))**0.5
-    q_rsl = -atan(2*r_traj/p_rsl) + atan((-r_traj*cos(lambdas)-r_traj)/(d+r_traj*sin(lambdas)))
-    t_rsl = - lambdas - atan(2*r_traj/p_rsl) + atan((-r_traj*cos(lambdas)-r_traj)/(d+r_traj*sin(lambdas)))
-    tau_rsl = r_traj*tan(gamma_traj*(lambdas+2*t_rsl)) + p_rsl*tan(gamma_g_traj)
+def _RSL(phi_init, d, r_traj, gamma_traj, gamma_g_traj, gammas_input):
+    gammas = atan(gammas_input)
+    lambdas = gammas-phi_init
+    mus = gammas
+    
+    p_rsl = (abs(-2*r_traj**2 + d**2 + cos(lambdas-mus)*2*r_traj**2 + 2*r_traj*d*(sin(lambdas)+ sin(mus))))**0.5
+    q_rsl = - mus - atan(2*r_traj/p_rsl) + atan((-r_traj*cos(lambdas)-r_traj*cos(mus))/(d+r_traj*sin(lambdas)+r_traj*sin(mus)))
+    t_rsl = - lambdas - atan(2*r_traj/p_rsl) + atan((-r_traj*cos(lambdas)-r_traj*cos(mus))/(d+r_traj*sin(lambdas)+r_traj*sin(mus)))
+    
+    tau_rsl = r_traj*tan(gamma_traj)*(lambdas-mus+2*t_rsl) + p_rsl*tan(gamma_g_traj)
+    
     return tau_rsl, np.array([t_rsl/r_traj, p_rsl/r_traj, q_rsl/r_traj])
 
 #necessary system parameters
@@ -24,22 +30,22 @@ gamma_g = -25.2 #deg
 psi_dot_max = 30 #deg/s
 
 #boundary conditions
-init_height = 10
+init_height = 70
 final_conditions = np.array([0,0,0])
 wind = _Gen_Wind_Field(2,2)
 tau_f = init_height
 
 #initial values of time changing variables
 gamma = 0
-sigma = 1
+sigma = pi/6
 V = 0
 #tau = init_height
 x_dot = 0
 y_dot = 0
 phi_dot = 0
 init_conditions = np.array([
-    -20, #-abs(tau_f*wind[0]/(V_g*sin(gamma_g))) - 10,
-    -20, #abs(tau_f*wind[1]/(V_g*sin(gamma_g))) - 10,
+    -10, #-abs(tau_f*wind[0]/(V_g*sin(gamma_g))) - 10,
+    -10, #abs(tau_f*wind[1]/(V_g*sin(gamma_g))) - 10,
     0
     ])
 
@@ -50,120 +56,102 @@ dt = 0.01
 tau_min = 0.01
 sim = True
 
-while sim:
-    
-    gamma = atan(tan(gamma_g)/cos(sigma))
-    V = sqrt(V_g**2 * cos(gamma) / (cos(gamma_g)*cos(sigma)))
+gamma_min = atan(tan(gamma_g)/cos(pi/6))
+V_min = sqrt(V_g**2 * cos(gamma_min) / (cos(gamma_g)*cos(pi/6)))
+r_min = V_min**2 * cos(gamma_min) / (9.81* tan(pi/6))
+tau_full = abs(2*pi*r_min/(V_min*cos(gamma_min)) * V_min * sin(gamma_min))
 
-    r = V**2 * cos(gamma) / (9.81* tan(sigma))
-    
-    tau_min = _RSL(init_conditions[2], 
-    sqrt(abs(init_conditions[1])**2 + abs(init_conditions[0])**2),
-    r, gamma, gamma_g
+tau_min = _RSL(init_conditions[2], 
+    sqrt((init_conditions[1])**2 + (init_conditions[0])**2),
+    r_min, gamma_min, gamma_g, init_conditions[1]/init_conditions[0]
     )[0]
 
-    arclengths = _RSL(init_conditions[2], 
-    sqrt(abs(init_conditions[1])**2 + abs(init_conditions[0])**2),
-    r, gamma, gamma_g
-    )[1]
+print(tau_min)
+print(tau_full)
+print(tau_f)
 
-    #parameters that change with time
-    if abs(tau_min) > 1.01*tau_f:
-        sigma += 0.001
-    elif abs(tau_min) < 0.99*tau_f:
-        sigma -= 0.001
-    else:
-        sim = False 
-        print(sigma)
-        print(r)
+eta = (tau_f - tau_min)/tau_full
 
+print(eta)
+print(eta)
+
+if eta > 0 and tau_min>0:
+    while sim:
+        
+        gamma = atan(tan(gamma_g)/cos(sigma))
+        V = sqrt(V_g**2 * cos(gamma) / (cos(gamma_g)*cos(sigma)))
+
+        r = V**2 * cos(gamma) / (9.81* tan(sigma))
+        
+        tau = _RSL(init_conditions[2], 
+        sqrt(abs(init_conditions[1])**2 + abs(init_conditions[0])**2),
+        r, gamma, gamma_g, init_conditions[1]/init_conditions[0]
+        )[0]
+
+        arclengths = _RSL(init_conditions[2], 
+        sqrt(abs(init_conditions[1])**2 + abs(init_conditions[0])**2),
+        r, gamma, gamma_g, init_conditions[1]/init_conditions[0]
+        )[1]
+
+        #parameters that change with time
+        if tau > 1.01*tau_f:
+            sigma += 0.001
+        elif tau < 0.99*tau_f:
+            sigma -= 0.001
+        else:
+            sim = False 
+            print(sigma)
+            print(r)
+
+        
+        #tau += -V*sin(gamma) * dt
+        
+        '''a_tilda = -1/(tan(gamma))
+        u = 1/(r*cos(gamma))
+
+        init_conditions = init_conditions + np.array([
+            a_tilda * cos(gamma),
+            a_tilda * sin(gamma),
+            a_tilda * u
+        ])'''
+
+        '''
+        init_conditions = init_conditions + np.array([
+            -(cos(init_conditions[2])/tan(gamma) + wind[0]/V*sin(gamma)),
+            -(sin(init_conditions[2])/tan(gamma) + wind[1]/V*sin(gamma)),
+            -9.81*tan(sigma)/(V**2*sin(gamma))
+        ])
+
+        phi_dot = 9.81 * tan(sigma) / V'''
+
+    #total_length = V * cos(gamma) * init_height #/(V*sin(gamma))
+
+    arclengths = arclengths
+
+    print(arclengths)
+
+
+    x_1l = init_conditions[0] - r*sin(init_conditions[2]+arclengths[0]) + r*sin(init_conditions[2])
+    y_1l = init_conditions[1] + r*cos(init_conditions[2]+arclengths[0]) - r*cos(init_conditions[2])
+    phi_1l = init_conditions[2]+arclengths[0]
+
+    x_2l = x_1l+arclengths[1]*cos(phi_1l)
+    y_2l = y_1l+arclengths[1]*sin(phi_1l)
+    phi_2l = phi_1l
+
+    x_3l = x_2l + r*sin(init_conditions[2]-arclengths[2]) - r*sin(init_conditions[2])
+    y_3l = y_2l - r*cos(init_conditions[2]-arclengths[2]) + r*cos(init_conditions[2])
+    phi_3l = phi_1l - arclengths[2]
+
+    update = [init_conditions[0],x_1l, x_2l, x_3l]
+
+    update_2 = [init_conditions[1],y_1l, y_2l, y_3l]
+
+    plt.plot(init_conditions[0], init_conditions[1], 'bo')
+    plt.plot(update,update_2, 'r')
     
-    #tau += -V*sin(gamma) * dt
-    
-    '''a_tilda = -1/(tan(gamma))
-    u = 1/(r*cos(gamma))
+    #plt.plot(x_2l,y_2l, 'bo')
+    #plt.plot(x_3l,y_3l, 'bo')
 
-    init_conditions = init_conditions + np.array([
-        a_tilda * cos(gamma),
-        a_tilda * sin(gamma),
-        a_tilda * u
-    ])'''
-
-    '''
-    init_conditions = init_conditions + np.array([
-        -(cos(init_conditions[2])/tan(gamma) + wind[0]/V*sin(gamma)),
-        -(sin(init_conditions[2])/tan(gamma) + wind[1]/V*sin(gamma)),
-        -9.81*tan(sigma)/(V**2*sin(gamma))
-    ])
-
-    phi_dot = 9.81 * tan(sigma) / V'''
-
-total_length = V * cos(gamma) * init_height #/(V*sin(gamma))
-
-arclengths = arclengths*init_height
-
-print(arclengths)
-
-x_1l = []
-y_1l = []
-
-x_2l = []
-y_2l = []
-
-x_3l = []
-y_3l = []
-
-dtheta_r = -arclengths[0]
-theta_r = pi + dtheta_r
-dtheta_l = -arclengths[2]
-theta_l= -pi/2 + dtheta_l
-
-print(dtheta_l)
-print(dtheta_r)
-
-def func_1():
-    my_one = 0
-    interval_one = -pi/500#-arclengths[0]/(1000*r)
-    my_theta_r = pi
-    while my_one < 1000:
-        x_1l.append(init_conditions[0] + r + r*cos(my_theta_r))
-        y_1l.append(init_conditions[1] + r*sin(my_theta_r))
-        my_theta_r += interval_one
-        my_one += 1
-
-def func_2():
-    my_two = 0
-    interval_two = -pi/500#-arclengths[2]/(1000*r)
-    my_theta_l = -pi/2
-    while my_two < 1000:
-        x_2l.append(r*cos(my_theta_l ))
-        y_2l.append(r + r*sin(my_theta_l))
-        my_theta_l += interval_two
-        my_two += 1
-
-def func_3():
-    dy = y_2l[-1] - y_1l[-1]
-    dx = x_2l[-1] - x_1l[-1]
-    a = dy/dx
-    b = y_1l[-1] - dy/dx * x_1l[-1]
-
-    x_33 = x_1l[-1]
-
-    my_three = 0
-    interval_three = dx/1000
-    while my_three < 1000:
-        y_3l.append(+dy/dx*x_33+b)
-        x_3l.append(x_33)
-        x_33 += interval_three
-        my_three += 1
-
-func_1()
-func_2()
-func_3()
-
-plt.plot(x_1l,y_1l, 'r')
-plt.plot(x_2l,y_2l, 'r')
-plt.plot(x_3l,y_3l, 'r')
-
-# show the plot
-plt.show()
+    # show the plot
+    plt.show()
