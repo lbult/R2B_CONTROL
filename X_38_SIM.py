0df27504c3ac2f90da2ebfa2a39750c8d9535a3e
wingos80 <wingyc80@gmail.com>
2021-02-15 11:33:20 +0100

    Added modelling of parafoil pitching motion, the parafoil function "_Parafoil_Forces_Moments" belongin gto the ParafoilProperties class has got 2 new arguments; D_s (drag force of payload_), and m_s(mass of payload).

--- X_38_SIM.py
+++ X_38_SIM.py
@@ -6,7 +6,7 @@ from SelfFly import ParafoilProperties, Quaternion, Dynamics, Variable, Payload
 
 ts = 0.025
 #define parafoil-payload system properties
-parafoil = ParafoilProperties(m=635, alpha_0=(-3.5*pi/180), surface=697, a_0=6.41, Cd0=0.005,rigging=0)
+parafoil = ParafoilProperties(m=635, alpha_0=(-3.5*pi/180), surface=697, a_0=6.41, Cd0=0.005,rigging=0, ts=ts)
 mpayload = Payload(M=9979)
 
 #start dynamics and attitude tracking
@@ -14,10 +14,11 @@ parafoil_dynamics = Dynamics(dt=ts, mass=(parafoil.m+mpayload.M))
 parafoil_attitude = Quaternion()
 
 #system two
-parafoil2 = ParafoilProperties(m=635, alpha_0=(-3.5*pi/180), surface=697, a_0=6.41, Cd0=0.005, rigging=10*pi/180)
+parafoil2 = ParafoilProperties(m=635, alpha_0=(-3.5*pi/180), surface=697, a_0=6.41, Cd0=0.005, rigging=10*pi/180, ts=ts)
 parafoil_dynamics_2 = Dynamics(dt=ts, mass=(parafoil2.m+mpayload.M))
 
 #intialize variables to be tracked
+alpa = Variable("alpha")
 pos_x = Variable("Position [x]")
 pos_y = Variable("Position [y]")
 pos_z = Variable("Position [z]")
@@ -48,14 +49,14 @@ while start == True or pos_z.history[-1] > 0:
     parafoil_attitude._to_euler()
 
     #update parafoil forces and moments
-    Parafoil_Vector = parafoil._Parafoil_Forces_Moments(parafoil_dynamics.vel)
     Payload_Vector = mpayload._Calc_Forces(parafoil_dynamics.vel)
+    Parafoil_Vector = parafoil._Parafoil_Forces_Moments(parafoil_dynamics.vel, Payload_Vector, mpayload.M)
     Gravity_Vector = parafoil_attitude.body_g * 9.80665 * parafoil_dynamics.mass
     parafoil_dynamics.forces = Parafoil_Vector + Payload_Vector + Gravity_Vector
     
     #second parafoil
-    Parafoil_Vector_2 = parafoil2._Parafoil_Forces_Moments(parafoil_dynamics_2.vel)
     Payload_Vector_2 = mpayload._Calc_Forces(parafoil_dynamics_2.vel)
+    Parafoil_Vector_2 = parafoil2._Parafoil_Forces_Moments(parafoil_dynamics_2.vel, Payload_Vector_2, mpayload.M)
     Gravity_Vector_2 = parafoil_attitude.body_g * 9.80665 * parafoil_dynamics.mass
     parafoil_dynamics_2.forces = Parafoil_Vector_2 + Payload_Vector_2 + Gravity_Vector_2
 
@@ -65,7 +66,10 @@ while start == True or pos_z.history[-1] > 0:
     # input them into Dynamics
     parafoil_dynamics.update_dynamics(parafoil_attitude._rot_b_v())
     parafoil_dynamics._next_time_step()
-    
+
+    #update alpha var
+    alpa.update_history(parafoil.alpa)
+
     #update position vars
     pos_x.update_history(parafoil_dynamics.pos[0])
     pos_y.update_history(parafoil_dynamics.pos[1])
@@ -85,13 +89,14 @@ while start == True or pos_z.history[-1] > 0:
     ts+= 0.025
     start=False
 
-print(ts)
+
 
 fig = plt.figure()
 plt.plot(vel_x.history)
 plt.plot(vel_x_2.history)
 plt.show()
 
+alpa.plot(None, None, "y", alpa.var_name)
 pos_x.plot(pos_y.history, None, "x", pos_y.var_name)
 pos_z.plot(pos_x.history, None, "y", pos_x.var_name)
 vel_x.plot(None, None, "y", vel_x.var_name)
